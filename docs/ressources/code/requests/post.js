08c09c1bbcacc1c1561381f7b5c8b891604583b2
Samy Pessé <samy.pesse@friendco.de>
2013-09-01 19:40:36 +0200

    Correct error in code example

--- docs/ressources/code/requests/post.js
+++ docs/ressources/code/requests/post.js
@@ -1,4 +1,4 @@
-yapp.Requests.get("mybackend/test.php").then(function() {
+yapp.Requests.post("mybackend/test.php").then(function() {
     alert("Weird ... it wasn't suppose to work");
 }, function() {
     alert("Error ! (don't worry it's normal ;) )");
