0216e024cfc795ff847e55f101dd1e8d33804976
Samy Pessé <samypesse@gmail.com>
2013-12-08 19:32:17 +0100

    Bump version to 0.2.7

--- src/configs.js
+++ src/configs.js
@@ -7,7 +7,7 @@ define(function() {
         "args": {},
 
         // Hr version
-        "version": "0.2.5",
+        "version": "0.2.7",
 
         // Log level
         // "log", "debug", "warn", "error", "none"
