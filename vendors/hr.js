246c2b3400f32af88d86415ed3478eb4db51b57f
Samy Pessé <samypesse@gmail.com>
2013-12-15 02:12:18 +0100

    Bump version to 0.2.8

--- vendors/hr.js
+++ vendors/hr.js
@@ -10225,7 +10225,7 @@ define('hr/configs',[],function() {
         "args": {},
 
         // Hr version
-        "version": "0.2.7",
+        "version": "0.2.8",
 
         // Log level
         // "log", "debug", "warn", "error", "none"
