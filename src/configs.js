c4cd4d3e204fa4270b56db0ae2fdfdfbb483268e
Samy Pessé <samypesse@gmail.com>
2014-01-28 13:54:07 +0100

    Bump version to 0.5.3

--- src/configs.js
+++ src/configs.js
@@ -7,7 +7,7 @@ define(function() {
         "args": {},
 
         // Hr version
-        "version": "0.5.2",
+        "version": "0.5.3",
 
         // Log level
         // "log", "debug", "warn", "error", "none"
--- vendors/hr.js
+++ vendors/hr.js
@@ -10454,7 +10454,7 @@ define('hr/configs',[],function() {
         "args": {},
 
         // Hr version
-        "version": "0.5.2",
+        "version": "0.5.3",
 
         // Log level
         // "log", "debug", "warn", "error", "none"
